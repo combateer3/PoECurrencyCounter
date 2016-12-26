29b074d3f5b4d171cc8b1c14929a669887d40b1d
Scott <scottcorbat@yahoo.com>
2016-12-26 15:13:24 -0500

    added basic gui

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../gui/mainGUI.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Path of Exile Currency Counter");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -1,4 +1,116 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class MainController {
+
+    @FXML
+    private ChoiceBox zonesBox;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void addTransmute() {
+
+    }
+
+    @FXML
+    private void removeTransmute() {
+
+    }
+
+    @FXML
+    private void addAugment() {
+
+    }
+
+    @FXML
+    private void removeAugment() {
+
+    }
+
+    @FXML
+    private void addAlt() {
+
+    }
+
+    @FXML
+    private void removeAlt() {
+
+    }
+
+    @FXML
+    private void addAlch() {
+
+    }
+
+    @FXML
+    private void removeAlch() {
+
+    }
+
+    @FXML
+    private void addRegal() {
+
+    }
+
+    @FXML
+    private void removeRegal() {
+
+    }
+
+    @FXML
+    private void addChaos() {
+
+    }
+
+    @FXML
+    private void removeChaos() {
+
+    }
+
+    @FXML
+    private void addExalt() {
+
+    }
+
+    @FXML
+    private void removeExalt() {
+
+    }
+
+    @FXML
+    private void addDivine() {
+
+    }
+
+    @FXML
+    private void removeDivine() {
+
+    }
+
+    @FXML
+    private void addMirror() {
+
+    }
+
+    @FXML
+    private void removeMirror() {
+
+    }
+
+    @FXML
+    private void addSilver() {
+
+    }
+
+    @FXML
+    private void removeSilver() {
+
+    }
+
 }
