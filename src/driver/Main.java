6b27e9cf806eb59f3d78886252f17fefef93ed1b
Scott <scottcorbat@yahoo.com>
2016-12-27 17:55:08 -0500

    fixed main file path to work with artifact

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../gui/mainGUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/mainGUI.fxml"));
         primaryStage.setTitle("Path of Exile Currency Counter");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
